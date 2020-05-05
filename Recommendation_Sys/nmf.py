d7ffc20aafa2806e5b9cca000a123bf31c438e48
VietNDuc <viet.nguyen1123@gmail.com>
2020-05-06 05:08:32 +0700

    Fix function bug

--- Recommendation_Sys/nmf.py
+++ Recommendation_Sys/nmf.py
@@ -5,9 +5,24 @@ class NMF(object):
     """Nonnegative Matrix Factorization
     """
     def __init__(self,):
+        super(NMF, self).__init__()
+    
+    def loss(self):
+        """
+        """
+
+    def updateX(self):
+        """
+        """
+
+    def updateW(self):
+        """
+        """
         
-    def train(self):
-    
     def fit(self):
-    
+        """
+        """
+
     def predict(self):
+        """
+        """
--- Recommendation_Sys/svd.py
+++ Recommendation_Sys/svd.py
@@ -2,25 +2,29 @@ import numpy as np
 
 
 class SVD(object):
-    """Matrix factorization based on svd algo
+    """Matrix factorization based on funkSVD
     """
-    def __init__(self, matrix, K, lam = 0.1, Xinit = None, Winit = None, 
-                 learning_rate = 0.5, max_iter = 1000, print_every = True, user_based = 0, ):
+    def __init__(self, df, K, lambd=0.1, Xinit=None,
+                 Winit=None, lr_rate=0.5, max_iter=1000,
+                 verbose=True, user_based=True, use_biased=None):
         super(SVD, self).__init__()
-
-        self.raw = matrix.copy()
-        self.data = matrix.copy()
+        
+        df = df.values  # to matrix
+        df[:2, :] -= 1
+        self.raw = df.copy()  # for evaluation
+        self.data = df.copy()  # for training
         self.K = K
         self.lambd = lambd
         self.lr_rate = lr_rate
         self.max_iter = max_iter
         self.verbose = verbose
         self.user_based = user_based
+        self.use_biased = use_biased
 
-        self.n_users = int(np.max(Y_data[:, 0])) + 1 
-        self.n_items = int(np.max(Y_data[:, 1])) + 1
-        self.n_ratings = Y_data.shape[0]
-        self.mu = 0
+        self.n_users = int(np.max(df[:, 0])) + 1 
+        self.n_items = int(np.max(df[:, 1])) + 1
+        self.n_ratings = len(df)
+        self.global_mean = np.mean(df[:, 2])
 
         if Xinit is None: 
             self.X = np.random.randn(self.n_items, K)
@@ -32,14 +36,14 @@ class SVD(object):
         else: 
             self.W = Winit
         
-        # item biases
-        self.b = np.random.randn(self.n_items)
-        self.d = np.random.randn(self.n_users)
+        # biases for item, user
+        if use_biased:
+            self.b_i = np.random.randn(self.n_items)
+            self.d_u = np.random.randn(self.n_users)
 
 
-    def normalize(self):
-        """Normalize matrix before training
-        
+    def normalization(self):
+        """Normalize (user or item) before training
         """
         if self.user_based:
             user_col = 0
@@ -50,141 +54,164 @@ class SVD(object):
             item_col = 0 
             n_objects = self.n_items
 
-        users = self.Y_data[:, user_col] 
-        self.muu = np.zeros((n_objects,))
+        users = self.data[:, user_col] 
+        self.keep_mean = np.zeros((n_objects,))
         for n in range(n_objects):
-            # row indices of rating done by user n
-            # since indices need to be integers, we need to convert
             ids = np.where(users == n)[0].astype(np.int32)
-            # indices of all ratings associated with user n
-            item_ids = self.Y_data[ids, item_col] 
-            # and the corresponding ratings 
-            ratings = self.Y_data[ids, 2]
-            # take mean
-            m = np.mean(ratings) 
-#             print m
+            ratings = self.data[ids, 2]
+            m = np.nanmean(ratings) 
             if np.isnan(m):
-                m = 0 # to avoid empty array and nan value
-            self.muu[n] = m
-            # normalize
-            self.Y_data[ids, 2] = ratings - m
+                m = 0
+            self.keep_mean[n] = m
+
+            self.data[ids, 2] = ratings - m
             
             
     def loss(self):
+        """Calculate loss function
+        """
         L = 0 
-        for i in xrange(self.n_ratings):
-            # user, item, rating
-            n, m, rate = int(self.Y_data[i, 0]), int(self.Y_data[i, 1]), self.Y_data[i, 2]
-            L += 0.5*(self.X[m, :].dot(self.W[:, n]) + self.b[m] + self.d[n] + self.mu - rate)**2
-            
-        # regularization, don't ever forget this 
-        L /= self.n_ratings
-        L += 0.5*self.lam*(np.linalg.norm(self.X, 'fro') + np.linalg.norm(self.W, 'fro') + \
-                          np.linalg.norm(self.b) + np.linalg.norm(self.d))
-        return L 
+        if self.use_biased:
+            for i in range(self.n_ratings):
+                # user, item, rating
+                n, m, rate = int(self.data[i, 0]), int(self.data[i, 1]), self.data[i, 2]
+                L += 0.5*(self.X[m, :].dot(self.W[:, n]) + \
+                          self.b_i[m] + self.d_u[n] + self.global_mean - rate)**2
+                L /= self.n_ratings
+                L += 0.5*self.lam*(np.linalg.norm(self.X, 'fro') + np.linalg.norm(self.W, 'fro'))
+        else:
+            for i in range(self.n_ratings):
+                n, m, rate = int(self.data[i, 0]), int(self.data[i, 1]), self.data[i, 2]
+                L += 0.5*(rate - self.X[m, :].dot(self.W[:, n]))**2
+                L /= self.n_ratings
+                L += 0.5*self.lambd*(np.linalg.norm(self.X, 'fro') + \
+                                     np.linalg.norm(self.W, 'fro') + \
+                                     np.linalg.norm(self.b) + \
+                                     np.linalg.norm(self.d)
+                                    )
 
+        return L 
     
     def get_items_rated_by_user(self, user_id):
         """
-        get all items which are rated by user n, and the corresponding ratings
         """
-        # y = self.Y_data_n[:,0] # all users (may be duplicated)
-        # item indices rated by user_id
-        # we need to +1 to user_id since in the rate_matrix, id starts from 1 
-        # while index in python starts from 0
-        ids = np.where(self.Y_data[:,0] == user_id)[0] 
-        item_ids = self.Y_data[ids, 1].astype(np.int32) # index starts from 0 
-        ratings = self.Y_data[ids, 2]
+        ids = np.where(self.data[:,0] == user_id)[0] 
+        item_ids = self.data[ids, 1].astype(np.int32)
+        ratings = self.data[ids, 2]
         return (item_ids, ratings)
         
         
     def get_users_who_rate_item(self, item_id):
         """
-        get all users who rated item m and get the corresponding ratings
         """
-        ids = np.where(self.Y_data[:,1] == item_id)[0] 
-        user_ids = self.Y_data[ids, 0].astype(np.int32)
-        ratings = self.Y_data[ids, 2]
+        ids = np.where(self.data[:,1] == item_id)[0] 
+        user_ids = self.data[ids, 0].astype(np.int32)
+        ratings = self.data[ids, 2]
         return (user_ids, ratings)
-        
+
+
     def updateX(self):
-        for m in xrange(self.n_items):
-            user_ids, ratings = self.get_users_who_rate_item(m)
+        """Backprop and update X using gradient descent
+        """
+        if self.use_biased:
+            for m in range(self.n_items):
+                user_ids, ratings = self.get_users_who_rate_item(m)
+
+                Wm = self.W[:, user_ids]
+                dm = self.d_u[user_ids]
+                xm = self.X[m, :]
             
-            Wm = self.W[:, user_ids]
-            dm = self.d[user_ids]
-            xm = self.X[m, :]
-            
-            error = xm.dot(Wm) + self.b[m] + dm + self.mu - ratings 
-            
-            grad_xm = error.dot(Wm.T)/self.n_ratings + self.lam*xm
-            grad_bm = np.sum(error)/self.n_ratings + self.lam*self.b[m]
-            self.X[m, :] -= self.learning_rate*grad_xm.reshape((self.K,))
-            self.b[m]    -= self.learning_rate*grad_bm
-    
+                error = xm.dot(Wm) + self.b[m] + dm + self.global_mean - ratings 
+
+                grad_xm = error.dot(Wm.T)/self.n_ratings + self.lambd * xm
+                grad_bm = np.sum(error)/self.n_ratings + self.lambd*self.b[m]
+                self.X[m, :] -= self.lr_rate*grad_xm.reshape((self.K,))
+                self.b[m] -= self.learning_rate*grad_bm
+        else:
+            for m in range(self.n_items):
+                user_ids, ratings = self.get_users_who_rate_item(m)
+
+                Wm = self.W[:, user_ids]
+                grad_xm = -(ratings - self.X[m, :].dot(Wm)).dot(Wm.T)/self.n_ratings + \
+                                               self.lambd*self.X[m, :]
+                self.X[m, :] -= self.lr_rate*grad_xm.reshape((self.K,))
+
+
     def updateW(self):
-        for n in xrange(self.n_users):
-            item_ids, ratings = self.get_items_rated_by_user(n)
-            Xn = self.X[item_ids, :]
-            bn = self.b[item_ids]
-            wn = self.W[:, n]
-            
-            error = Xn.dot(wn) + bn + self.mu + self.d[n] - ratings
-            grad_wn = Xn.T.dot(error)/self.n_ratings + self.lam*wn
-            grad_dn = np.sum(error)/self.n_ratings + self.lam*self.d[n]
-            self.W[:, n] -= self.learning_rate*grad_wn.reshape((self.K,))
-            self.d[n]    -= self.learning_rate*grad_dn
-    
+        """Backprop and update W using gradient descent
+        """
+        if self.use_biased:
+            for n in range(self.n_users):
+                item_ids, ratings = self.get_items_rated_by_user(n)
+                Xn = self.X[item_ids, :]
+                bn = self.b[item_ids]
+                wn = self.W[:, n]
+
+                error = Xn.dot(wn) + bn + self.global_mean + self.d[n] - ratings
+                grad_wn = Xn.T.dot(error) / self.n_ratings + self.lambd * wn
+                grad_dn = np.sum(error) / self.n_ratings + self.lambd * self.d[n]
+                self.W[:, n] -= self.lr_rate * grad_wn.reshape((self.K,))
+                self.d[n] -= self.lr_rate * grad_dn
+        else:
+            for n in range(self.n_users):
+                item_ids, ratings = self.get_items_rated_by_user(n)
+                Xn = self.X[item_ids, :]
+
+                grad_wn = -Xn.T.dot(ratings - Xn.dot(self.W[:, n]))/self.n_ratings + \
+                            self.lambd * self.W[:, n]
+                self.W[:, n] -= self.lr_rate*grad_wn.reshape((self.K,))
+
+                
     def fit(self):
-        self.normalize_Y()
-        for it in xrange(self.max_iter):
+        """Training phase
+        """
+        self.normalization()
+        for it in range(self.max_iter):
             self.updateX()
             self.updateW()
-            if (it + 1) % self.print_every == 0:
-                rmse_train = self.evaluate_RMSE(self.Y_raw)
-                print 'iter =', it + 1, ', loss =', self.loss(), ', RMSE train =', rmse_train
+            if self.verbose & ((it+1) % 10 == 0):
+                rmse_train = self.evaluate(self.raw)
+                print('iter =', it + 1, ', loss =', self.loss(), ', RMSE train =', rmse_train)
     
     
-    def pred(self, u, i):
-        """ 
-        predict the rating of user u for item i 
-        if you need the un
+    def predict(self, u, i):
+        """Prediction given user_id and item_id
         """
-        u = int(u)
-        i = int(i)
-        if self.user_based == 1:
-            bias = self.muu[u]
+        if self.user_based:
+            bias = self.keep_mean[u]
         else:
-            bias = self.muu[i]
+            bias = self.keep_mean[i]
+        if self.use_biased:
+            pred = self.X[i, :].dot(self.W[:, u]) + self.b_i[i] + self.d_u[u] + bias
+        else:
+            pred = self.X[i, :].dot(self.W[:, u]) + bias
         
-        pred = self.X[i, :].dot(self.W[:, u]) + self.b[i] + self.d[u] + bias
-#         if pred < 0:
-#             return 0 
-#         if pred > 5: 
-#             return 5 
-#         return pred 
         return max(0, min(5, pred))
         
     
-    def pred_for_user(self, user_id):
-        ids = np.where(self.Y_data_n[:, 0] == user_id)[0]
-        items_rated_by_u = self.Y_data_n[ids, 1].tolist()              
+    def predict_for_user(self, user_id):
+        """Prediction given user_id
+        """
+        ids = np.where(self.data[:, 0] == user_id)[0]
+        items_rated_by_u = self.data[ids, 1].tolist()              
         
         y_pred = self.X.dot(self.W[:, user_id])
         predicted_ratings= []
-        for i in xrange(self.n_items):
+        for i in range(self.n_items):
             if i not in items_rated_by_u:
                 predicted_ratings.append((i, y_pred[i]))
         
         return predicted_ratings
     
-    def evaluate_RMSE(self, rate_test):
-        n_tests = rate_test.shape[0]
-        SE = 0 # squared error
-        for n in xrange(n_tests):
-            pred = self.pred(rate_test[n, 0], rate_test[n, 1])
-#             print pred, rate_test[n, 2]
-            SE += (pred - rate_test[n, 2])**2 
+    def evaluate(self, test_set):
+        """Evaluation (RMSE/ MSE metric)
+        """
+        n_tests = test_set.shape[0]
+        SE = 0
+        for n in range(n_tests):
+            pred = self.predict(test_set[n, 0], test_set[n, 1])
+            SE += (pred - test_set[n, 2])**2 
 
         RMSE = np.sqrt(SE/n_tests)
+
         return RMSE
